2d566ac5124d6048010a3d04b206a7232b6ec3c0
Lực THQ <triplesix0209@gmail.com>
2026-06-11 20:28:03 +0700

    Update Triboulet's Apology text for improved shield display formatting

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -218,7 +218,7 @@ return {
                     "Each {C:attention}King{} and {C:attention}Queen{} held in hand gives {X:mult,C:white}X/2{} Mult.",
                     "If about to be destroyed, prevent it once per round",
                     "and permanently add {X:mult,C:white}X1{} Mult to its {X:mult,C:white}X{} Mult.",
-                    "{C:inactive}(Currently {X:mult,C:white}X#1#{C:inactive} Mult, Shield: #5#/1){}"
+                    "{C:inactive}(Currently {X:mult,C:white}X#1#{C:inactive} Mult, Shield: {C:attention}#5#{}){}"
 				},
 			},
 		},
